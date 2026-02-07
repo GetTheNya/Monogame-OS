e2598cf0d28fb5da8153004157210dff5803e0b1
GetTheNya <romaro422@gmail.com>
2026-02-07 23:33:04 +0200

    Project build improvements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿using var game = new TheGame.Game1();
+﻿using System.Runtime.Loader;
+using System.IO;
+using System;
+
+AssemblyLoadContext.Default.Resolving += (context, assemblyName) => {
+    string expectedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libs", $"{assemblyName.Name}.dll");
+    if (File.Exists(expectedPath)) {
+        return context.LoadFromAssemblyPath(expectedPath);
+    }
+    return null;
+};
+
+using var game = new TheGame.Game1();
 game.Run();
